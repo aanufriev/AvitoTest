7d614b62b9a7cacfa3429f0bbf4588a00096f6c0
aanufriev <anufriev.artem.mail@gmail.com>
2020-08-18 16:54:05 +0300

    Add connection to database and database init

--- storage/storage.go
+++ storage/storage.go
@@ -1,6 +1,15 @@
 package storage
 
-import "github.com/aanufriev/AvitoTest/models"
+import (
+	"database/sql"
+	"io/ioutil"
+
+	"github.com/aanufriev/AvitoTest/models"
+)
+
+const (
+	postgres = "postgres"
+)
 
 type StorageInterface interface {
 	SaveUser(user *models.User) (int, error)
@@ -8,4 +17,36 @@ type StorageInterface interface {
 	SaveMessage(message *models.Message) (int, error)
 	GetChats(userID string) ([]models.Chat, error)
 	GetMessages(chatID string) ([]models.Message, error)
+}
+
+type PostgresStorage struct {
+	db *sql.DB
+}
+
+func (ps *PostgresStorage) Open(dataSourceName string) error {
+	db, err := sql.Open(postgres, dataSourceName)
+	if err != nil {
+		return err
+	}
+	ps.db = db
+
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ps PostgresStorage) InitDatabase() error {
+	file, err := ioutil.ReadFile("./storage/init.sql")
+	if err != nil {
+		return err
+	}
+
+	_, err = ps.db.Query(string(file))
+	if err != nil {
+		return err
+	}
+	return nil
 }

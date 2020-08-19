37040166dc7837b479c903b2f643207ce5a3e410
aanufriev <anufriev.artem.mail@gmail.com>
2020-08-19 13:59:38 +0300

    Add methods to save chat and message in postgres

--- storage/storage.go
+++ storage/storage.go
@@ -68,6 +68,42 @@ func (ps PostgresStorage) SaveUser(user *models.User) (int, error) {
 	return lastID, nil
 }
 
+func (ps PostgresStorage) SaveChat(chat *models.Chat) (int, error) {
+	if len(chat.Users) < 2 {
+		return 0, fmt.Errorf("SaveChat error: Expect users count: > 2\nGot users count: %v", len(chat.Users))
+	}
+
+	for _, userID := range chat.Users {
+		if !ps.checkUser(userID) {
+			return 0, fmt.Errorf("SaveChat error: User with id %v doesn`t exist", userID)
+		}
+	}
+
+	var lastID int
+
+	err := ps.db.QueryRow(
+		"INSERT INTO chats (name, created_at) VALUES ($1, $2) RETURNING id",
+		chat.Name, chat.CreatedAt,
+	).Scan(&lastID)
+
+	if err != nil {
+		return 0, fmt.Errorf("SaveChat error: %s with chat: %v", err, chat)
+	}
+	chat.ID = lastID
+
+	for _, userID := range chat.Users {
+		_, err = ps.db.Exec(
+			"INSERT INTO userchat (user_id, chat_id) VALUES ($1, $2)",
+			userID, chat.ID,
+		)
+		if err != nil {
+			return 0, fmt.Errorf("SaveChat error: %s with user_id: %s", err, userID)
+		}
+	}
+
+	return lastID, nil
+}
+
 func (ps PostgresStorage) checkUser(userID string) bool {
 	user := models.User{}
 
@@ -83,6 +119,30 @@ func (ps PostgresStorage) checkUser(userID string) bool {
 	default:
 		panic(err)
 	}
+}
+
+func (ps PostgresStorage) SaveMessage(msg *models.Message) (int, error) {
+	if !ps.checkUser(msg.AuthorID) {
+		return 0, fmt.Errorf("SaveMessage error: User with id %v doesn`t exist", msg.AuthorID)
+	}
+
+	if !ps.checkChat(msg.ChatID) {
+		return 0, fmt.Errorf("SaveMessage error: Chat with id %v doesn`t exist", msg.ChatID)
+	}
+
+	var lastID int
+
+	err := ps.db.QueryRow(
+		"INSERT INTO messages (chat_id, user_id, msg_text, created_at) VALUES ($1, $2, $3, $4) RETURNING id",
+		msg.ChatID, msg.AuthorID, msg.Text, msg.CreatedAt,
+	).Scan(&lastID)
+
+	if err != nil {
+		return 0, fmt.Errorf("SaveMessage error: %s with message: %v", err, msg)
+	}
+	msg.ID = lastID
+
+	return lastID, err
 }
 
 func (ps PostgresStorage) checkChat(chatID string) bool {
